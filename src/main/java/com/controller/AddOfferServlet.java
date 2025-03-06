8ad1ab4e18e63f9e935c3567d4e63ce73e8e239a
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-03-06 13:59:50 +0000

    job offre

--- src/main/java/com/controller/AddOfferServlet.java
+++ src/main/java/com/controller/AddOfferServlet.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import com.model.JobOffer;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +9,8 @@ public class AddOfferServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response){
         String title = request.getParameter("jobTitle");
         String description = request.getParameter("jobDescription");
-
+        JobOffer jobOffer = new JobOffer();
+        jobOffer.setTitle(title);
+        jobOffer.setDescription(description);
     }
 }
---
+++ src/main/java/com/model/JobOffer.java
@@ -0,0 +1,23 @@
+package com.model;
+
+public class JobOffer {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String title;
+    private String description;
+
+}
